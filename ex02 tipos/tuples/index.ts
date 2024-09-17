330b2b35a048f429fd40d721ecffa3c0b70e7ddb
Clenilton Costa <clenilton.costa24@hotmail.com>
2024-09-16 22:12:19 -0300

    Create index.ts

---
+++ ex02 tipos/tuples/index.ts
@@ -0,0 +1,3 @@
+let itens:[string, number, boolean] = ['Samsung',1500, true]
+// para que eu possa armazenar mais de um tipo em uma variavel, eu utilizo a tuple, mas para que nao ocorra nenhum erro, preciso seguir a ordem que predefinir pra cada tipo como é mostrado no exemplo acima
+ 
