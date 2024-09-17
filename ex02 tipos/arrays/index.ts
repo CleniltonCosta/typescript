8fce61836190bb0018bfc62ebee0e1628020e872
Clenilton Costa <clenilton.costa24@hotmail.com>
2024-09-16 22:01:34 -0300

    Create index.ts

---
+++ ex02 tipos/arrays/index.ts
@@ -0,0 +1,7 @@
+let idade: number[]=[18,30,67]
+
+
+let nomes: string[]=['roberto', 'anderson', 4]
+
+
+// toda vez que uma array tiver um tipo especifico, tenha certeza em dizer o tipo dela antes. Como n o exemplo acima, na variavel nomes. No momento em que tipei ela a string, ela me reclamou n o momento em que coloquei um number.
