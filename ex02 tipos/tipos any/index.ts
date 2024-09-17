60bb2fa6594e0e3bca8ed5ae403767b5c3f5d355
Clenilton Costa <clenilton.costa24@hotmail.com>
2024-09-16 21:53:27 -0300

    Update index.ts

---
+++ ex02 tipos/tipos any/index.ts
@@ -0,0 +1,8 @@
+let carro = 'classic'
+let portas = 4
+let estoque = true
+
+let preco
+
+
+// no momento em que nao defino um tipo especifico a uma variavel, consequentemente mais pra frente poderemos usar ela associar ela a algum outro tipo futuramente. Tendo em vista que so podera ser usado em parte especificas com bastante cuidado
