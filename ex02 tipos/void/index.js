a5ca0e804bc4f4424eea4d46d456e0e45e9c2f28
Clenilton Costa <clenilton.costa24@hotmail.com>
2024-09-18 20:31:55 -0300

    criando funcao void]

---
+++ ex02 tipos/void/index.js
@@ -0,0 +1,7 @@
+function soma(num1, num2) {
+    return num1 + num2;
+}
+function preco(valor) {
+    console.log('o preco é' + valor);
+}
+preco(soma(1, 4));
---
+++ ex02 tipos/void/index.ts
@@ -0,0 +1,9 @@
+function soma(num1:number, num2:number):number{
+    return num1+num2
+}
+
+function preco(valor){
+    console.log('o preco é' + valor)
+}
+
+preco(soma(1,4))
