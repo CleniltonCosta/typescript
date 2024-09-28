e3fd8b487463f4d3fcf65ec1b4d42b51892ba911
Clenilton Costa <clenilton.costa24@hotmail.com>
2024-09-28 20:13:43 -0300

    literal type
    l

---
+++ EX03/literal type/index.ts
@@ -0,0 +1,9 @@
+let produtoEstoque: 10| 20 | 50
+
+let produtoTamanho: 'pp'| 'p' | 'm' | 'g' | 'gg'
+
+produtoEstoque= 10
+
+produtoTamanho = 'm'
+
+// literal type é um tipo que permite especificar valores exatos, em vez de aceitar qualquer tipo generico. No exemplo acima especifiquei cada valor de cada variavel para que ela nao aceite qualquer valor number ou qualquer string.
